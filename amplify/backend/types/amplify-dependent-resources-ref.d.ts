76220f4ee11528617de5587ea9749b5f0ad4333e
Espe01 <favoritefood6@gmail.com>
2023-11-27 11:49:52 -0500

    Added graphical and storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "s3fa917212": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
